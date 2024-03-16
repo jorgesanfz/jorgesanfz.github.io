cd8a37b737fe9006742dac28d99e50da0028d4bd
jorgesanfz <jorge@sanfz.eu>
2024-03-16 20:24:17 +0100

    adj

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.body.addEventListener("click", function (event) {
-  console.log(event);
+  /*console.log(event);
+
+  if (event.target.tagName === "A" && event.target.href.match(/\/(?!\/)/)) {
+    event.preventDefault();
+    window.history.pushState(null, "", event.target.href);
+    window.dispatchEvent(new Event("popstate"));
+  }*/
 });
